c0a30daa7e6f9be4bd807455d8f078bb78d0190a
wentao.ma <wentao.ma@dianping.com>
2013-10-14 18:41:47 +0800

    FIX bug

--- c_guidtouserid.sql
+++ c_guidtouserid.sql
@@ -13,11 +13,11 @@ sort by guid_str ,c desc
 ;
 
 insert overwrite table guiduseridcount
-select guid_str, user_id, count(*) as c from guiduseridcount
+select guid, userid, sum(count) as c from guiduseridcount
 group by guid, userid
 having c>1
-distribute by guid_str
-sort by guid_str ,c desc
+distribute by guid
+sort by guid ,c desc
 ;
 
 -- old = new
@@ -29,7 +29,7 @@ select * from mainuserid_new
 insert overwrite table mainuserid_new
 select guid ,userid from (
 select guid, userid, count
-from guiduseridcount 
+from guiduseridcount
 distribute by guid
 sort by guid,count desc ) a
 where row_number(guid)=1 
